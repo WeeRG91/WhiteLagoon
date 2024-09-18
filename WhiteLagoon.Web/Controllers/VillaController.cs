2069c13f24d7cd96ddae35d903cd43d1889984e4
Thibaut Gobert <35538385+thibeDev@users.noreply.github.com>
2024-09-18 12:53:21 +0200

    Create/Read/Update/Delete

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("description", "The description cannot exactly match the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(villa => villa.Id == villaId);
+            //Villa? obj = _db.Villas.Find(villaId);
+            //var VillaList = _db.Villas.Where(villa => villa.Price > 50 && villa.Occupancy > 0).FirstOrDefault();
+
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("description", "The description cannot exactly match the name");
+            }
+
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+
+            return View();
+        }
+        
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(villa => villa.Id == villaId);
+            //Villa? obj = _db.Villas.Find(villaId);
+            //var VillaList = _db.Villas.Where(villa => villa.Price > 50 && villa.Occupancy > 0).FirstOrDefault();
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(villa => villa.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+
+            return View();
+        }
+    }
+}
